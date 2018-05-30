a664a41db031f66c282c6f5029c1683fc69d7b37
Sam Sha <sam_sha@163.com>
2018-05-30 18:24:43 +0800

    gulp-aes(key)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var PluginError = gutil.PluginError;
 var aes = require('./lib/aes');
 const PLUGIN_NAME = 'gulp-aes';
 
-function gulpcrypto() {
+function gulpcrypto(key) {
   var stream = through.obj(function (file, enc, cb) {
     if (file.isStream()) {
       this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
@@ -13,6 +13,9 @@ function gulpcrypto() {
     }
 
     if (file.isBuffer()) {
+      if (typeof key !== 'undefined') {
+        aes.key = key;
+      }
       file.contents = new Buffer(aes.enc(file.contents));
     }
 
